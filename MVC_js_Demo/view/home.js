6aa15d47282bf1816064806fcf9ab5de9ab641d3
Mythili1507 <ymythili1507@gmail.com>
2023-09-09 11:59:38 +0530

    Enhanced styles and designs

--- MVC_js_Demo/view/home.js
+++ MVC_js_Demo/view/home.js
@@ -3,13 +3,31 @@ function getHomeView(modelData)
     let template =
     `
     <div>
-        <h1 class="home-heading">Fruit deliver to your home 🚚</h1>
-      
-        <button class="browse-btn" onClick="changeViewWithMVC('browseProducts')">Browse products</button>
-       
+
+    <h1 class="home-desc"> Fruits are available in this online shop. Buy fresh & healthy fruits online at best price from this
+     best fruit shop &
+    fill your fruit basket </h1>
+
+     <button class="browse-btn" onClick="changeViewWithMVC('browseProducts')">Browse products</button>
+
+     <h3 class="why-sec-main-heading">Why should you select The Fruit Garden?</h3>
+     <p class="why-desc"> Fruits are nature's gift to mankind. These are not only delicious and refreshing but are also the chief source of vitamins, minerals and proteins. These constituents are essential for the normal physiological well being and help in maintaining healthy state through development of
+     resistance against pathogens </p>
+
+    <h5 class="why-sec-heading"> 20+ Years of Experience</h5>
+
+    <p class="why-sec-desc"> 20+ years of challenges, renewal, changes, we faced all of that made our distinctive mark</p>
+
+    <h5 class="why-sec-heading">We Provide Fresh Quality Fruits</h5>
+    <p class="why-sec-desc"> High quality, health and deliciousness are our quality</p>
+
+    <h5 class="why-sec-heading">Our Worldwide Suppilers</h5>
+
+    <p class="why-sec-desc"> We bring fruits of the world to you</p>
+
     </div>
     `;
-    
+{/* <button class="browse-btn" onClick="changeViewWithMVC('browseProducts')">Browse products</button> */}
     return template;
 }
 
